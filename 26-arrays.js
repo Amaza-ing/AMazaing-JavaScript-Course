9f359c29d0d62b92db1bef9214a5253793225cae
Adrián Maza <adri_mv1@hotmail.com>
2025-06-02 12:11:28 +0200

    26-arrays

---
+++ 26-arrays.js
@@ -0,0 +1,34 @@
+const numbers = [10, 20, 30, 40, 50];
+console.log(numbers);
+console.log(numbers[0]);
+console.log(numbers.length);
+console.log(numbers[numbers.length - 1]);
+
+const fruits = ["manzana", "plátano", "kiwi"];
+console.log(fruits);
+console.log(fruits[0]);
+
+const books = [];
+books[0] = "Harry Potter";
+console.log(books);
+
+const products = [
+  {
+    name: "Ordenador",
+    price: 1000,
+    isForSale: true,
+  },
+  {
+    name: "Teclado",
+    price: 50,
+    isForSale: true,
+  },
+  {
+    name: "Ratón",
+    price: 20,
+    isForSale: true,
+  },
+];
+
+console.log(products);
+console.log(products[0]);
