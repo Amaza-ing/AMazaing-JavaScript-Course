7c7e58fe7ddfac71abe54b388afd71a14dac676e
Adrián Maza <adri_mv1@hotmail.com>
2025-06-02 10:07:11 +0200

    24-objects

---
+++ 24-objetos.js
@@ -0,0 +1,29 @@
+const product = {
+  name: "Computer",
+  price: 1000,
+  isForSale: true,
+};
+
+console.log(product);
+console.log(product.name);
+console.log(product["price"]);
+
+const key = "isForSale";
+console.log(product[key]);
+
+
+product.color = "Blue";
+console.log(product);
+
+
+const calculator = {
+  sum: function (a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+};
+
+console.log(calculator.sum(10, 5));
+console.log(calculator.subtract(5, 3));
