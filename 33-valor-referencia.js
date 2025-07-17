ec6ea1b6873093e746f83ba7e1fb11f513ce9037
Adrián Maza <adri_mv1@hotmail.com>
2025-07-17 12:02:37 +0200

    JS 33 added

---
+++ 33-valor-referencia.js
@@ -0,0 +1,34 @@
+let a = 10;
+let b = a;
+
+console.log(a);
+console.log(b);
+
+a = 20;
+
+console.log(a);
+console.log(b);
+
+
+const o1 = { name: "Adrián"};
+const o2 = o1;
+
+console.log(o1);
+console.log(o2);
+
+o1.name = "María";
+
+console.log(o1);
+console.log(o2);
+
+// o1 = { name: "Carlos"};
+
+// console.log(o1);
+// console.log(o2);
+
+
+
+
+
+
+
